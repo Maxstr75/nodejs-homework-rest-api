f896e9850fa4ebf3802b0c09ff8e3fa48809c2de
Maxim Stribylo <max057521@gmail.com>
2022-09-20 22:24:14 +0300

    update

---
+++ middlewares/validarion.js
@@ -0,0 +1,49 @@
+const Joi = require("joi");
+
+// Схема валидации создания контакта
+const addContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "org", "ca", "co.uk"] },
+    })
+    .required(),
+  phone: Joi.string()
+    .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
+    .required(),
+});
+
+// Схема валидации обновления контакта
+const putContactSchema = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).optional(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net", "org", "ca", "co.uk"] },
+    })
+    .optional(),
+  phone: Joi.string()
+    .pattern(/^([+]?\d{1,2}[-\s]?|)\d{3}[-\s]?\d{3}[-\s]?\d{4}$/)
+    .optional(),
+}).min(1);
+
+// Обработка ошибок валидации
+const validate = (schema, res, obj, next) => {
+  const validationLogs = schema.validate(obj);
+  if (validationLogs.error) {
+    return res
+      .status(400)
+      .json({ message: validationLogs.error.replace(/"/g, "") });
+  }
+  next();
+};
+
+module.exports = {
+  addContactValidation: (req, res, next) => {
+    return validate(addContactSchema, res, req.body, next);
+  },
+  putContactValidation: (req, res, next) => {
+    return validate(putContactSchema, res, req.body, next);
+  },
+};
