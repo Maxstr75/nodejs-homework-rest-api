392af0e13c944678d42c9e7f7c44944b16b3e57f
Maxim Stribylo <max057521@gmail.com>
2022-09-26 19:56:32 +0300

    update
    update

---
+++ routes/api/contacts (2).js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  getContacts,
+  getContactsById,
+  addContacts,
+  deleteContact,
+  putContact,
+} = require("../../controllers/contactsController");
+
+const {
+  addContactValidation,
+  putContactValidation,
+} = require("../../middlewares/validation");
+
+const { ctrlWrapper } = require("../../helpers/сtrlWrapper");
+
+// Роут для поиска всех контактов
+router.get("/", ctrlWrapper(getContacts));
+
+// Роут поиска контакта по id
+router.get("/:contactId", ctrlWrapper(getContactsById));
+
+// Роут для создания контакта
+router.post("/", addContactValidation, ctrlWrapper(addContacts));
+
+// Роут для удаления контакта
+router.delete("/:contactId", ctrlWrapper(deleteContact));
+
+// Роут для обновления контактов
+router.put("/:contactId", putContactValidation, ctrlWrapper(putContact));
+
+module.exports = router;
