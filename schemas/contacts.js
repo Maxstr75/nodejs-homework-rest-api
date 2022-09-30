7822078346e34b65bcff0f118fa0ee090b4af75b
Maxim Stribylo <max057521@gmail.com>
2022-09-30 23:03:03 +0300

    move DB_HOST to process.env

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,22 +1,27 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      require: [true, "Set name for contact!"],
+    },
+    email: {
+      type: String,
+      require: [true, "Email is required!"],
+    },
+    phone: {
+      type: String,
+      require: [true, "Phone is required!"],
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Contact = mongoose.model("contact", contactSchema);
 
