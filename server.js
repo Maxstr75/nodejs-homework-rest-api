bed200d5bedff2f19defb77bcc825bdef2860aba
Maxim Stribylo <max057521@gmail.com>
2022-09-30 23:57:57 +0300

    add .env.example

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-// const app = require("./app");
-// const db = require("./db");
-
-// const { PORT = 3000 } = process.env;
-
-// const start = async () => {
-//   try {
-//     await db();
-
-//     app.listen(PORT, () => {
-//       console.log(`Server running. Use our API on port ${PORT}`);
-//     });
-//   } catch (error) {
-//     console.log(`Server not running. Error message: ${error.message}`);
-//   }
-// };
-
-// start();
-
 const mongoose = require("mongoose");
 const app = require("./app");
 
@@ -37,3 +18,22 @@ mongoose
     console.log(error.message);
     process.exit(1);
   });
+
+// const app = require("./app");
+// const db = require("./db");
+
+// const { PORT = 3000 } = process.env;
+
+// const start = async () => {
+//   try {
+//     await db();
+
+//     app.listen(PORT, () => {
+//       console.log(`Server running. Use our API on port ${PORT}`);
+//     });
+//   } catch (error) {
+//     console.log(`Server not running. Error message: ${error.message}`);
+//   }
+// };
+
+// start();
