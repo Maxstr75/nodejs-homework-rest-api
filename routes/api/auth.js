d90a5ee86a69e442c52a59efcdfc121ed08ab52d
Maxim Stribylo <max057521@gmail.com>
2022-10-01 23:25:33 +0300

    update

---
+++ routes/api/auth.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const ctrlWrapper = require("../../helpers/сtrlWrapper");
+
+// singup
+
+router.post("/login", ctrlWrapper);
+
+module.exports = router;
