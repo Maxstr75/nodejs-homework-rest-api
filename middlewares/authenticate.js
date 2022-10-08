dbf1c5ad4e955aea2a5f7ae2ed5e0b69dae3e44e
Maxim Stribylo <max057521@gmail.com>
2022-10-09 00:05:08 +0300

    update

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -21,7 +21,7 @@ const authenticate = async (req, res, next) => {
     }
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findUserById(id);
-    if (!user) {
+    if (!user || !user.token) {
       return res.status(401).json({ message: "Not authorized" });
     }
     req.user = user;
