bc372f3a4e2a55541c9d911d800d01477d69de81
Maxim Stribylo <max057521@gmail.com>
2022-10-12 22:46:10 +0300

    update

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,15 +1,16 @@
 const multer = require("multer");
 const path = require("path");
 
+// Создаем временное хранилище файла
 const tempDir = path.join(__dirname, "../", "temp");
 // console.log(__dirname);
 const multerConfig = multer.diskStorage({
-  destination: tempDir,
+  destination: tempDir, // Путь к временному файлу
   filename: (req, file, cb) => {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // Под каким именем сохраняем файл
   },
   limits: {
-    fileSize: 2048,
+    fileSize: 2048, // Ограничение загрузки файла
   },
 });
 
