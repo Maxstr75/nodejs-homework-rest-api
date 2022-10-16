c4096217e961c5c2a594600dda27cb1e3b5e769e
Maxim Stribylo <max057521@gmail.com>
2022-10-16 20:17:44 +0300

    update

--- services/emailService.js
+++ services/emailService.js
@@ -4,14 +4,14 @@ const sgMail = require("@sendgrid/mail");
 const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
-const email = {
-  to: "gixin10127@inkmoto.com",
-  from: "max057521@gmail.com",
-  subject: "Новая заявка с сайта",
-  html: "<p>С сайта пришла новая заявка<p/>",
+const sendEmail = async (data) => {
+  const email = { ...data, from: "max057521@gmail.com" };
+  // eslint-disable-next-line no-useless-catch
+  try {
+    await sgMail.send(email);
+    return true;
+  } catch (error) {
+    throw error;
+  }
 };
-
-sgMail
-  .send(email)
-  .then(() => console.log("Email send success"))
-  .catch((error) => console.log(error.message));
+module.exports = sendEmail;
