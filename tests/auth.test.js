5d1695700c260e1ec0f515d8e2fdbb14388f9dc5
Maxim Stribylo <max057521@gmail.com>
2022-10-18 23:13:54 +0300

    update

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -3,40 +3,48 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = require("../app");
-// const { User } = require("../models/user");
+const { User } = require("../models/user");
 
 const { DB_HOST, PORT = 3000 } = process.env;
 
 describe("tests for login controller/signup", () => {
-  beforeAll(() =>
-    mongoose
-      .connect(DB_HOST)
-      .then(() => {
-        app.listen(PORT, () => {
-          console.log("DataBase connect success");
-          console.log(`Server running. Use our API on port: ${PORT}`);
-        });
-      })
-      .catch((error) => {
-        console.log(`Server is not running. Error message: ${error.message}`);
-        process.exit(1);
-      })
-  );
-  it("login returns response should have status code 200 and the response body should return a token", async () => {
-    const response = await request(app).post("/api/users/login").send({
+  let server;
+  beforeAll(() => (server = app.listen(PORT)));
+  afterAll(() => server.close());
+
+  beforeEach((done) => {
+    mongoose.connect(DB_HOST).then(() => done());
+  });
+  afterEach((done) => {
+    mongoose.connection.db.dropCollection(() => {
+      mongoose.connection.close(() => done());
+    });
+  });
+  it("login returns response should have status code 200 and the response body should return a token and user object with 2 fields email and subscription, both of data type String", async () => {
+    const newUser = {
       email: "maxim789@gmail.com",
       password: "Max785@",
-    });
+    };
+    const user = await User.create(newUser);
+
+    const loginUser = {
+      email: "maxim789@gmail.com",
+      password: "Max785@",
+    };
+
+    const response = await request(app)
+      .post("/api/users/login")
+      .send(loginUser);
     expect(response.statusCode).toBe(200);
-    expect(response.body.token).toBe("string");
-  });
-  it("signup returns status 201 a user object with 2 fields email and subscription, both of data type String", async () => {
-    const response = await request(app).post("/api/users/signup").send({
-      email: "max057521@gmail.com",
-      password: "Maxim1975@",
-    });
-    expect(response.statusCode).toBe(201);
-    expect(response.body.email).toBe("string");
-    expect(response.body.subscription).toBe("string");
+    const { body } = response;
+    expect(body.token).toByTruthy();
+    const { token } = await User.findById(user._id);
+    expect(body.token).toBe(token);
+    expect(body.email).toByTruthy();
+    const { email } = await User.findOne(user.email);
+    expect(body.email).toBe(email);
+    expect(body.subscription).toByTruthy();
+    const { subscription } = await User.findOne(user.id, user.subscription);
+    expect(body.subscription).toBe(subscription);
   });
 });
