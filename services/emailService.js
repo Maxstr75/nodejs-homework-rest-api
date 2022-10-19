a5ec83eabd2f853d29a81c53b56ded8ba76d6e91
Maxim Stribylo <max057521@gmail.com>
2022-10-19 20:36:48 +0300

    update

--- services/emailService.js
+++ services/emailService.js
@@ -36,11 +36,7 @@ transporter
 // const sendEmail = async (data) => {
 //   const email = { ...data, from: "max057521@gmail.com" };
 //   // eslint-disable-next-line no-useless-catch
-//   try {
 //     await sgMail.send(email);
 //     return true;
-//   } catch (error) {
-//     throw error;
-//   }
 // };
 module.exports = sendEmail;
