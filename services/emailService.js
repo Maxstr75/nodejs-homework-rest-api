aef77c02c89b66d730247035bb1b4ef231d76192
Maxim Stribylo <max057521@gmail.com>
2022-10-19 22:54:31 +0300

    update

--- services/emailService.js
+++ services/emailService.js
@@ -1,42 +1,51 @@
 // ---------- Отправка письма через nodemailer -------------------
-const nodemailer = require("nodemailer");
+// const nodemailer = require("nodemailer");
 
-const { META_PASSWORD } = process.env;
+// const { META_PASSWORD } = process.env;
 
-const nodemailerConfig = {
-  host: "smtp.meta.ua",
-  port: 465, // 25, 465 и 2255
-  secure: true,
-  auth: {
-    user: "maxim050775@meta.ua",
-    pass: META_PASSWORD,
-  },
-};
+// const nodemailerConfig = {
+//   host: "smtp.meta.ua",
+//   port: 465, // 25, 465 и 2255
+//   secure: true,
+//   auth: {
+//     user: "maxim050775@meta.ua",
+//     pass: META_PASSWORD,
+//   },
+// };
 
-const transporter = nodemailer.createTransport(nodemailerConfig);
+// const transporter = nodemailer.createTransport(nodemailerConfig);
 
-const sendEmail = {
-  to: "gixin10127@inkmoto.com",
-  from: "maxim050775@meta.ua",
-  subject: "Подтверждение email",
-  html: `<a target="_blank" href= "http://localhost:3000/api/users/verify/verificationToken"> Подтвердить email</a>`,
-};
+// const sendEmail = {
+//   to: "polaciw691@inkmoto.com",
+//   from: "maxim050775@meta.ua",
+//   subject: "Подтверждение email",
+//   html: `<a target="_blank" href= "http://localhost:3000/api/users/verify/verificationToken"> Подтвердить email</a>`,
+// };
 
-transporter
-  .sendMail(sendEmail)
-  .then(() => console.log("Email send success"))
-  .catch((error) => console.log(error.message));
+// transporter
+//   .sendMail(sendEmail)
+//   .then(() => console.log("Email send success"))
+//   .catch((error) => console.log(error.message));
 
 // -------- Отправка письма через Sendgrid -----------
-// const sgMail = require("@sendgrid/mail");
+const sgMail = require("@sendgrid/mail");
 
-// const { SENDGRID_API_KEY } = process.env;
-// sgMail.setApiKey(SENDGRID_API_KEY);
+const { SENDGRID_API_KEY } = process.env;
+sgMail.setApiKey(SENDGRID_API_KEY);
 
-// const sendEmail = async (data) => {
-//   const email = { ...data, from: "max057521@gmail.com" };
-//   // eslint-disable-next-line no-useless-catch
-//     await sgMail.send(email);
-//     return true;
-// };
+const sendEmail = {
+  to: "polaciw691@inkmoto.com",
+  from: "max057521@gmail.com",
+  subject: "Подтверждение email",
+  html: `<a href= "http://localhost:3000/api/users/verify/verificationToken"> Confirm email</a>`,
+};
+sgMail
+  .send(sendEmail)
+  .then(() => {
+    console.log("Email send success");
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
+
 module.exports = sendEmail;
