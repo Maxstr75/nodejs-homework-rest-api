7a5e6ff165bf57853daae5ba68a7e0d65186ae7a
Maxim Stribylo <max057521@gmail.com>
2022-10-20 21:31:51 +0300

    update

--- services/emailService.js
+++ services/emailService.js
@@ -1,32 +1,3 @@
-// ---------- Отправка письма через nodemailer -------------------
-// const nodemailer = require("nodemailer");
-
-// const { META_PASSWORD } = process.env;
-
-// const nodemailerConfig = {
-//   host: "smtp.meta.ua",
-//   port: 465, // 25, 465 и 2255
-//   secure: true,
-//   auth: {
-//     user: "maxim050775@meta.ua",
-//     pass: META_PASSWORD,
-//   },
-// };
-
-// const transporter = nodemailer.createTransport(nodemailerConfig);
-
-// const sendEmail = {
-//   to: "relero4914@imdutex.com",
-//   from: "maxim050775@meta.ua",
-//   subject: "Подтверждение email",
-//   html: `<a href= "http://localhost:3000/api/users/verify/verificationToken"> Подтвердить email</a>`,
-// };
-
-// transporter
-//   .sendMail(sendEmail)
-//   .then(() => console.log("Email send success"))
-//   .catch((error) => console.log(error.message));
-
 // -------- Отправка письма через Sendgrid -----------
 const sgMail = require("@sendgrid/mail");
 
@@ -34,7 +5,7 @@ const { SENDGRID_API_KEY } = process.env;
 sgMail.setApiKey(SENDGRID_API_KEY);
 
 const mail = {
-  to: "relero4914@imdutex.com",
+  to: "hiyig38620@cadolls.com",
   from: "max057521@gmail.com",
   subject: "Подтверждение email",
   html: `<a href= "http://localhost:3000/api/users/verify/verificationToken"> Confirm email</a>`,
@@ -53,5 +24,34 @@ const sendEmail = async (data) => {
   await sgMail.send(mail);
   return true;
 };
+
+// ---------- Отправка письма через nodemailer -------------------
+// const nodemailer = require("nodemailer");
+
+// const { META_PASSWORD } = process.env;
+
+// const nodemailerConfig = {
+//   host: "smtp.meta.ua",
+//   port: 465, // 25, 465 и 2255
+//   secure: true,
+//   auth: {
+//     user: "maxim050775@meta.ua",
+//     pass: META_PASSWORD,
+//   },
+// };
+
+// const transporter = nodemailer.createTransport(nodemailerConfig);
+
+// const sendEmail = {
+//   to: "hiyig38620@cadolls.com",
+//   from: "maxim050775@meta.ua",
+//   subject: "Подтверждение email",
+//   html: `<a href= "http://localhost:3000/api/users/verify/verificationToken"> Подтвердить email</a>`,
+// };
+
+// transporter
+//   .sendMail(sendEmail)
+//   .then(() => console.log("Email send success"))
+//   .catch((error) => console.log(error.message));
 
 module.exports = sendEmail;
