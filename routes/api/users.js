8398b857019ed3eb0dc7e0dcf439ac92e98de1b4
Maxim Stribylo <max057521@gmail.com>
2022-10-23 00:07:39 +0300

    update

--- routes/api/users.js
+++ routes/api/users.js
@@ -16,6 +16,7 @@ const {
 const {
   regLogValidation,
   subscriptionValidation,
+  reVerifyValidation,
 } = require("../../middlewares/userValidation");
 const { authenticate } = require("../../middlewares/authenticate");
 const ctrlWrapper = require("../../helpers/сtrlWrapper");
@@ -23,7 +24,7 @@ const upload = require("../../middlewares/upload");
 
 router.post("/signup", regLogValidation, ctrlWrapper(registerController)); // Роут для регистрации юзера
 router.get("/verify/:verificationToken", ctrlWrapper(verifyController)); // Роут для верификации юзера
-router.post("/verify/", ctrlWrapper(reVerifyController)); // Роут повторной отправки для верификации юзера
+router.post("/verify", reVerifyValidation, ctrlWrapper(reVerifyController)); // Роут повторной отправки для верификации юзера
 router.post("/login", regLogValidation, ctrlWrapper(loginController)); // Роут для входа юзера
 router.get("/logout", authenticate, ctrlWrapper(logoutController)); // Роут для выхода юзера
 router.get(
